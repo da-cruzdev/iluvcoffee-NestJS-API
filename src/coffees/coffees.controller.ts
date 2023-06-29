84f339c85c66f6112a09f179e953685409f7165d
Da-cruz <wilfrieddacruz@arolitec.com>
2023-06-29 11:28:51 +0000

    create coffee service

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
+  constructor(private readonly coffeeService: CoffeesService) {}
   @Get()
   getAll() {
     return 'this action returns all the coffees!!';
